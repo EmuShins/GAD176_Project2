105d3cd788908a02fe4b94f9634ee4c2ce7af5c4
SAE1\1033265 <SAE>
2025-03-24 14:10:43 +1030

    Setup file structure and added scriptable object.

---
+++ Assets/EmuScripts/ControllerScriptable.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="ScriptableController", menuName ="ScriptableObjects/Scriptable Controller")]
+public class ControllerScriptable : ScriptableObject
+{
+    public float mass;
+    public float moveSpeed;
+}
