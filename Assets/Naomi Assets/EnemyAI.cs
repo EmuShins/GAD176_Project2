2f143f77287e9f0de18e0261eac6d2a644e109dc
SAE1\1034391 <naomi.morandin@gmail.com>
2025-04-07 14:26:15 +0930

    Built folder structure for enemytypes, created 3 different enemytypes using scriptableobj, implemented speed and size values for enemy types

--- Assets/Naomi Assets/EnemyAI.cs
+++ Assets/Naomi Assets/EnemyAI.cs
@@ -1,7 +1,52 @@
+using TMPro;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyAI", menuName = "ScriptableObjects/EnemyAI")]
-public class EnemyAI : ScriptableObject
+public class EnemyAI : MonoBehaviour
 {
-    
+    public EnemyTypeValues enemyAI;
+    private float movementSpeed;
+    LayerMask layerMask = LayerMask.GetMask("Wall", "Character");
+    //On Validate, when the scriptable object is dragged onto the game object in the inspector this runs.
+    private void OnValidate()
+    {
+        //null check, then sets the attached game objects variables to match the enemy type to whatever number is set in the scriptable object.
+        if (enemyAI != null)
+        {
+            this.gameObject.transform.localScale = new Vector3(enemyAI.scale, enemyAI.scale, enemyAI.scale);
+            movementSpeed = enemyAI.movementSpeed;
+        }
+    }
+    void Start()
+    {
+        LayerMask layerMask = LayerMask.GetMask("Wall", "Character");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+      
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.forward * 100);
+    }
+
+
 }
---
+++ Assets/Naomi Assets/EnemyTypes/EnemyTypeScript/EnemyTypeValues.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyTypeValues", menuName = "ScriptableObjects/EnemyTypeValues")]
+public class EnemyTypeValues : ScriptableObject
+{
+    public float movementSpeed = 10;
+    public float scale = 1;  
+}
